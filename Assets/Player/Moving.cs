aa9b9b3aa4a1d732e4e7026ab909811129413ef8
LickMyPiyan <133853639+LickMyPiyan@users.noreply.github.com>
2025-03-01 19:18:58 +0800

    我把瞬移穿牆修好了操
    Dash();

--- Assets/Player/Moving.cs
+++ Assets/Player/Moving.cs
@@ -3,19 +3,28 @@ using UnityEngine;
 public class Moving : MonoBehaviour
 {
     public float PlayerSpeed = 1.0f;
-    public float DashDistance = 3.0f;
+    public float DashDistance = 0.5f;
     public float DashCooldown = 3.0f;
     public float DashTimer = 0.0f;
-        void Move()
+    public float PlayerWidth = 0.5f;
+
+    void Move()
     {
         transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * PlayerSpeed * Time.deltaTime;
     }
     void Dash()
     {
         Vector3 MousePos = new Vector3(Input.mousePosition.x - 960, Input.mousePosition.y - 540, 0);
-        if(Input.GetKeyDown(KeyCode.Space) && Time.time - DashTimer > DashCooldown)
+        Vector3 Posf = Vector3.Normalize(MousePos) * DashDistance;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Posf);
+        if (Input.GetKeyDown(KeyCode.Space) && hit.collider != null && Vector2.Distance(transform.position, hit.point) < DashDistance && Time.time - DashTimer > DashCooldown)
         {
-            transform.position += new Vector3(MousePos.x, MousePos.y, 0).normalized * DashDistance;
+            transform.position = new Vector3(hit.point.x, hit.point.y, 0);
+            DashTimer = Time.time;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && Time.time - DashTimer > DashCooldown)
+        {
+            transform.position += Posf;
             DashTimer = Time.time;
         }
     }
