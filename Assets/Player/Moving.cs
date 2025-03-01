ccd07f1a19880d697f2826e2a8e054f7b9902d03
LickMyPiyan <133853639+LickMyPiyan@users.noreply.github.com>
2025-03-01 16:19:54 +0800

    tag:wall

--- Assets/Player/Moving.cs
+++ Assets/Player/Moving.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 public class Moving : MonoBehaviour
 {
     public float PlayerSpeed = 1.0f;
-    public float DashDistance = 5.0f;
-    void Move()
+    public float DashDistance = 3.0f;
+    public float DashCooldown = 3.0f;
+    public float DashTimer = 0.0f;
+        void Move()
     {
         transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * PlayerSpeed * Time.deltaTime;
     }
     void Dash()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        Vector3 MousePos = new Vector3(Input.mousePosition.x - 960, Input.mousePosition.y - 540, 0);
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time - DashTimer > DashCooldown)
         {
-            transform.position += (Input.mousePosition - transform.position) / Vector2.Distance(Input.mousePosition, transform.position) * DashDistance;
+            transform.position += new Vector3(MousePos.x, MousePos.y, 0).normalized * DashDistance;
+            DashTimer = Time.time;
         }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
