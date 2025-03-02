a0b0a53483b1bb9441ffda9a00df52d93fb5e0f3
OwO00364 <0966jeff626@gmail.com>
2025-03-02 14:58:48 +0800

    加註解

--- Assets/Player/AtkVeiw.cs
+++ Assets/Player/AtkVeiw.cs
@@ -7,17 +7,20 @@ public class AtkVeiw : MonoBehaviour
     public float Zrotation;
     void rotate()
     {
+        //搞到滑鼠對應遊戲內的座標
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         MouseXvalue = mousePosition.x;
         MouseYvalue = mousePosition.y;
+        //算z軸角度tan值
+        Zrotation = (MouseYvalue - transform.position.y) / (MouseXvalue - transform.position.x);
+        //滑鼠在角色右側
         if (MouseXvalue - transform.position.x > 0)
         {
-            Zrotation = (MouseYvalue - transform.position.y) / (MouseXvalue - transform.position.x);
             transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan(Zrotation)/Mathf.PI*180 - 135);
         }
+        //滑鼠在角色左側
         else
         {
-            Zrotation = (MouseYvalue - transform.position.y) / (MouseXvalue - transform.position.x);
             transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan(Zrotation)/Mathf.PI*180 + 45);
         }
     }
