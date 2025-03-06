54483c6b649f6c9b0551fe632522d17c2e2934c9
OwO00364 <0966jeff626@gmail.com>
2025-03-06 21:58:51 +0800

    地圖拖曳
    天才一般的

--- Assets/UI/UIManagerM.cs
+++ Assets/UI/UIManagerM.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 public class UIManagerM : MonoBehaviour
 {
     public GameObject StartUI;
+    public GameObject Battle01;
+    public GameObject EnterB01;
+    public Vector3 MousePosI;
+    public Vector3 MousePosF;
+    public Vector3 minCameraPosition = new Vector3( 0 , 0, -10);
+    public Vector3 maxCameraPosition = new Vector3( 10 , 6, -10);
+
+    Camera MainCamera;
+
 
     public void BattlePressed()
     {
@@ -21,17 +30,48 @@ public class UIManagerM : MonoBehaviour
         SceneManager.LoadScene("BattleP01");
     }
 
+    void Follow()
+    {
+        Vector3 screenPosition = MainCamera.WorldToScreenPoint(Battle01.transform.position);
+        EnterB01.GetComponent<RectTransform>().position = screenPosition;
+    }
+
+    void CameraMove()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MousePosI = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            MousePosF = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+            MainCamera.transform.position += MousePosI - MousePosF;
+
+            MainCamera.transform.position = new Vector3
+            (
+                Mathf.Clamp(MainCamera.transform.position.x, minCameraPosition.x, maxCameraPosition.x),
+                Mathf.Clamp(MainCamera.transform.position.y, minCameraPosition.y, maxCameraPosition.y),
+                Mathf.Clamp(MainCamera.transform.position.z, minCameraPosition.z, maxCameraPosition.z)
+            );
+        }
+
+    }
+
     void Start()
     {
         StartUI.SetActive(false);
+
+        MainCamera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             BattleUnPressed();
         }
+        Follow();
+        CameraMove();
     }
 }
